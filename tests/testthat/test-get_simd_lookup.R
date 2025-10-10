8972d22a9c17db33dab6c073da37330aa8b64217
Ken O'Neill <Ken.O'Neill@gov.scot>
2025-10-10 10:07:23 +0100

    Allow two tests to skip GitHub Actions CI for R

--- tests/testthat/test-get_simd_lookup.R
+++ tests/testthat/test-get_simd_lookup.R
@@ -1,4 +1,9 @@
 test_that("lookup table for simd made", {
+  # `get_simd_lookup()` relies on a live SPARQL endpoint and then downloads
+  # the file from its URL -- this would fail some GitHub Actions for R. So,
+  # skipping CI if it comes to this.
+  testthat::skip_on_ci()
+
   simd_data <- get_simd_lookup()
 
   # check if result is a dataframe
